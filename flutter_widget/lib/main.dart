75a52512ec5f0c2796268967dc6904249ec29f22
MoonCheung <ikmoons.is@gmail.com>
2021-10-13 00:35:29 +0800

    chore: init flutter_widget

---
+++ flutter_widget/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import './fooderlich_theme.dart';
+import './home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // 此小部件是应用的根源
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create theme
+    final theme = FooderlichTheme.light();
+
+    // TODO: Apply Home widget
+    return MaterialApp(
+      title: 'Fooderlich',
+      theme: theme,
+      // Scaffold: 脚手架
+      home: const Home(),
+    );
+  }
+}
