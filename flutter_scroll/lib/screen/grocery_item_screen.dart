7eeee3e2627802c7b3782071b7bec643bc52d79e
MoonCheung <ikmoons.is@gmail.com>
2021-10-22 21:43:31 +0800

    chore: 新建grocery_item_screen文件

---
+++ flutter_scroll/lib/screen/grocery_item_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart'; //为您的应用提供材质颜色选择器
+import 'package:intl/intl.dart'; // 提供国际化和本地化实用程序。您将使用它来格式化日期
+import 'package:uuid/uuid.dart'; // 为每个杂货项目生成唯一的密钥。这有助于您了解要添加、更新或删除的项目
+import '../models/models.dart';
