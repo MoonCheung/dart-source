1d9e50e226018d81f77a2607d05dbdb638bc6460
MoonCheung <ikmoons.is@gmail.com>
2021-10-24 22:46:08 +0800

    feat:增加flutter国际配置且移除pubspec.lock

--- flutter_scroll/lib/main.dart
+++ flutter_scroll/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
@@ -19,6 +20,15 @@ class Fooderlich extends StatelessWidget {
       debugShowCheckedModeBanner: false, // 隐藏调试横幅
       theme: theme,
       title: 'Fooderlich',
+      localizationsDelegates: [
+        // Material Components库提供了本地化的字符串和其他值
+        GlobalMaterialLocalizations.delegate,
+        // 定义widget默认的文本方向，从左到右或从右到左。
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('zh', 'CH'), // 中文
+      ],
       // TODO 8: Replace Home with MultiProvider
       home: MultiProvider(
         providers: [
